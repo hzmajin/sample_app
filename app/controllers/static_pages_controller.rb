4a3da5e87918a82fab79f3a7fafd4be8c1e830b1
Hassan Majin <heraclituss@gmail.com>
2016-04-13 12:52:43 +0430

    this is modification of root action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
   end
-
   def help
   end
   def about
-      
+  end
+
+  def myaction
+    render text: "This is My World"
   end
 end
